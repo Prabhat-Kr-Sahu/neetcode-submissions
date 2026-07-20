673169eb863852e83e10a6445be624a6e621d100
Prabhat Kumar Sahu <prabhatkr.sahu190@gmail.com>
2026-07-20 19:50:08 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int  n = prices.size(); 
+        vector<vector<int>> dp(n , vector<int> ( 3, -1));
+        function<int(int, int)> f = [&](int idx ,int b){
+            if(idx == n){
+                return 0;
+            }
+            if(dp[idx][b] != -1) return dp[idx][b];
+            if(b == 1){
+                int skip = f(idx +1 , b);
+
+                int sell = prices[idx] + f(idx+1, 2);
+                return dp[idx][b] = max(sell , skip);
+            }
+            else if(b == 0){
+                int skip = f(idx+1, b);
+                int buy = -prices[idx] + f(idx +1, 1);
+                return dp[idx][b] = max(buy, skip );
+            }else{
+                return dp[idx][b] = 0;
+            }
+        };
+        return  f(0,0);
+    }
+};

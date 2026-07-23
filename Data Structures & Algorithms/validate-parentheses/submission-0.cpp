007bef2d9d1e51cba3799fc49c81cf84d7295d72
Prabhat Kumar Sahu <prabhatkr.sahu190@gmail.com>
2026-07-23 08:49:24 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto c : s){
+            if(c == '{' || c == '[' || c == '('){
+                st.push(c);
+            }
+            else if(c == ')'){
+                if( !st.empty() && st.top() == '('){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(c == ']'){
+                if( !st.empty() && st.top() == '['){
+                    st.pop();
+                }
+                 else{
+                    return false;
+                }
+            }
+            else if(c == '}'){
+                if( !st.empty() &&  st.top() == '{'){
+                    st.pop();
+                }
+                 else{
+                    return false;
+                }
+            }
+        }
+        return (st.empty() ? true : false);
+    }
+};
